74d6eb6a8455d10c131ed565f6b44bb3b9289e0f
Adrian Rodriguez <adrian.rodriguez7109@gmail.com>
2022-05-13 15:19:06 -0500

    Configure mailgun

--- config/services.php
+++ config/services.php
@@ -17,8 +17,6 @@ return [
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
     ],
 
     'postmark' => [
